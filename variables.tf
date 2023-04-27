a95fb6ab982c7c22bbb82ef11c1ad1f725add907
SDSnyk <101288071+SDSnyk@users.noreply.github.com>
2023-04-26 21:07:45 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 #Company Name
 
 #CHANGE THIS - lower case only
-variable "victim_company" {
+variable "victim_company_me" {
   type        = string
   description = "For naming purposes"
   default     = "testpatch"
 }
 
 #CHANGE THIS
-variable "owner" {
+variable "stefan" {
   type        = string
   description = "For Tagging and Filtering purposes"
   default     = "Patch"
